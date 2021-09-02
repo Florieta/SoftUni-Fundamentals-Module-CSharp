c2bc2a22bd7b2eaaa686cd267bebe41253673937
Florieta <florieta.pencheva@gmail.com>
2021-09-02 13:38:00 +0300

    Added some of the solutions of the lab and exercises about Objects and Classes

---
+++ 06.ObjectAndClasses/RandomizeWords/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace RandomizeWords
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine().Split(' ');
+
+            Random rnd = new Random();
+
+            for (int pos1 = 0; pos1 < words.Length; pos1++)
+
+            {
+
+                int pos2 = rnd.Next(words.Length);
+
+                string word = words[pos1];
+                words[pos1] = words[pos2];
+                words[pos2] = word;
+            }
+
+            Console.WriteLine(string.Join(Environment.NewLine, words));
+        }
+    }
+}
