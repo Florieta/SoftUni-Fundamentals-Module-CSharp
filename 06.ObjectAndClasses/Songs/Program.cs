8218e0bc71b1b9965e5058b0b87214c982b8b78a
Florieta <florieta.pencheva@gmail.com>
2021-09-04 15:59:13 +0300

    Added the problem solutions about Objects and Classes

---
+++ 06.ObjectAndClasses/Songs/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Songs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberSongs = int.Parse(Console.ReadLine());
+            List<Song> songs = new List<Song>();
+
+            for (int i = 0; i < numberSongs; i++)
+            {
+                string[] song = Console.ReadLine().Split("_");
+                Song newSong = new Song();
+                newSong.TypeList = song[0];
+                newSong.Name = song[1];
+                newSong.Time = song[2];
+                songs.Add(newSong);
+            }
+
+            string filter = Console.ReadLine();
+            if (filter == "all")
+            {
+                foreach (var song in songs)
+                {
+                    Console.WriteLine(song.Name);
+                }
+            }
+            else
+            {
+                foreach (var song in songs.Where(s => s.TypeList == filter))
+                {
+                    Console.WriteLine(song.Name);
+                }
+            }
+        }
+    }
+
+    class Song
+    {
+        public string TypeList { get; set; }
+        public string Name { get; set; }
+        public string Time { get; set; }
+    }
+}
---
+++ 06.ObjectAndClasses/Student/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Student
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            static void Main(string[] args)
+            {
+                bool end = false;
+                List<Students> students = new List<Students>();
+
+                AddStudents(end, students);
+
+                string filter = Console.ReadLine();
+                foreach (var student in students.Where(s => s.Hometown == filter))
+                {
+                    Console.WriteLine($"{student.FirstName} {student.SecondName} is {student.Age} years old.");
+                }
+            }
+
+            static void AddStudents(bool end, List<Students> students)
+            {
+                while (!end)
+                {
+                    string input = Console.ReadLine();
+                    if (input == "end")
+                    {
+                        end = true;
+                        continue;
+                    }
+
+                    string[] data = input.Split();
+                    Students student = new Students();
+
+                    student.FirstName = data[0];
+                    student.SecondName = data[1];
+                    student.Age = int.Parse(data[2]);
+                    student.Hometown = data[3];
+
+                    students.Add(student);
+                }
+            }
+        }
+
+        class Students
+        {
+            public string FirstName { get; set; }
+            public string SecondName { get; set; }
+            public int Age { get; set; }
+            public string Hometown { get; set; }
+        }
+    }
+}
