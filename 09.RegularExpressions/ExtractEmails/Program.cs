7b59ad0930fa878559cc3b3c206200b3117cea53
Florieta <florieta.pencheva@gmail.com>
2021-09-15 13:52:46 +0300

    Added some problem solutions about Regular Expressions

---
+++ 09.RegularExpressions/ExtractEmails/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace ExtractEmails
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"(?<=\s)([A-za-z0-9]+[-._]*[A-za-z0-9]+)@([A-Za-z]+(([.-]*)[A-Za-z]+)*\.[a-z]{2,})";
+
+            string text = Console.ReadLine();
+
+            MatchCollection matchCollection = Regex.Matches(text, pattern);
+
+            foreach (Match item in matchCollection)
+            {
+                Console.WriteLine(item.Value);
+            }
+        }
+    }
+}
---
+++ 09.RegularExpressions/Furniture/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Furniture
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"^>>([a-zA-Z]+)<<([0-9]+\.{0,1}[0-9]{0,})!([0-9]+)";
+
+            string input = Console.ReadLine();
+            decimal totalPrice = 0;
+            Console.WriteLine("Bought furniture:");
+
+            while (input != "Purchase")
+            {
+                Match match = Regex.Match(input, pattern);
+
+                if(!match.Success)
+                {
+                    input = Console.ReadLine();
+                    continue;
+                }
+                string productName = match.Groups[1].Value;
+                decimal productPrice = decimal.Parse(match.Groups[2].Value);
+                int productCount = int.Parse(match.Groups[3].Value);
+
+                totalPrice += productPrice * productCount;
+
+                
+                Console.WriteLine(productName);
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine($"Total money spend: {totalPrice:F2}");
+        }
+    }
+}
